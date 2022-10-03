4bc9b28db87e1cbe4890d4dd0c32a299c97b9e9a
khoafrancisco <114103847+khoafrancisco@users.noreply.github.com>
2022-10-03 15:03:58 +0700

    Update L06P05.cpp

--- L06P05.cpp
+++ L06P05.cpp
@@ -1,1 +1,28 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n, a[1000][1000];
+    cin >> m >> n;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            cin >> a[i][j];
+
+    int maxEven = 0;
+    int index = -1;
+
+    for (int i = 0; i < m; i++) {
+        int cnt = 0;
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] % 2 == 0)
+                cnt += 1;
+        }
+        if (cnt > maxEven) {
+            index = i;
+            maxEven = cnt;
+        }
+    }
+    cout << index;
+    return 0;
+}
