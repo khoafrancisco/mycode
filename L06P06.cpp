13c031ad88386aca3c1f046266e21d9171f248b2
khoafrancisco <114103847+khoafrancisco@users.noreply.github.com>
2022-10-03 15:05:12 +0700

    Update L06P06.cpp

--- L06P06.cpp
+++ L06P06.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    int m, n, a[1000][1000];
+    cin >> m >> n;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            cin >> a[i][j];
+    int cnt = 0;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            if (a[i][j] > 100)
+                cnt++;
+    cout << cnt;
+    return 0;
+}
